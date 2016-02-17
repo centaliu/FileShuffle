e8fa8fed7fa8e0c67872aca9076a2b244323e0c1
Centa Liu <centaliu@gmail.com>
2016-02-17 13:55:36 +0800

    initialization

---
+++ FileShuffle/frmMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FileShuffle
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnSelDir = new System.Windows.Forms.Button();
+			this.txtPath = new System.Windows.Forms.TextBox();
+			this.folderSel = new System.Windows.Forms.FolderBrowserDialog();
+			this.btnShuffle = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnSelDir
+			// 
+			this.btnSelDir.Location = new System.Drawing.Point(503, 23);
+			this.btnSelDir.Name = "btnSelDir";
+			this.btnSelDir.Size = new System.Drawing.Size(65, 24);
+			this.btnSelDir.TabIndex = 0;
+			this.btnSelDir.Text = "...";
+			this.btnSelDir.UseVisualStyleBackColor = true;
+			this.btnSelDir.Click += new System.EventHandler(this.btnSelDir_Click);
+			// 
+			// txtPath
+			// 
+			this.txtPath.Location = new System.Drawing.Point(26, 23);
+			this.txtPath.Name = "txtPath";
+			this.txtPath.Size = new System.Drawing.Size(460, 22);
+			this.txtPath.TabIndex = 1;
+			this.txtPath.Text = "D:\\TMPFolder\\";
+			// 
+			// btnShuffle
+			// 
+			this.btnShuffle.Location = new System.Drawing.Point(574, 23);
+			this.btnShuffle.Name = "btnShuffle";
+			this.btnShuffle.Size = new System.Drawing.Size(60, 24);
+			this.btnShuffle.TabIndex = 2;
+			this.btnShuffle.Text = "Shffule";
+			this.btnShuffle.UseVisualStyleBackColor = true;
+			this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(658, 118);
+			this.Controls.Add(this.btnShuffle);
+			this.Controls.Add(this.txtPath);
+			this.Controls.Add(this.btnSelDir);
+			this.MaximizeBox = false;
+			this.Name = "frmMain";
+			this.Text = "FileShuffle";
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnSelDir;
+		private System.Windows.Forms.TextBox txtPath;
+		private System.Windows.Forms.FolderBrowserDialog folderSel;
+		private System.Windows.Forms.Button btnShuffle;
+	}
+}
+
---
+++ FileShuffle/frmMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileShuffle
+{
+	public partial class frmMain : Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+		private int exchTimes = 100;
+
+		private List<int> nums = new List<int>();
+		private List<string> mp3Files = new List<string>();
+
+		private void btnSelDir_Click(object sender, EventArgs e)
+		{
+			folderSel.ShowNewFolderButton = false;
+			folderSel.RootFolder = Environment.SpecialFolder.MyComputer;
+			DialogResult res = folderSel.ShowDialog();
+			if (res == DialogResult.OK) txtPath.Text = folderSel.SelectedPath;
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnShuffle_Click(object sender, EventArgs e)
+		{
+			nums.Clear();
+			mp3Files.Clear();
+			// step 1: get all filenames;
+			if (txtPath.Text.Substring(txtPath.Text.Length - 2, 1) != @"\") txtPath.Text = txtPath.Text + @"\";
+			string[] allFiles = Directory.GetFiles(txtPath.Text);
+			// step 2: fulfill a list nums of sequential integers, and a list of all file names in the folder
+			foreach (string fn in allFiles)
+			{
+				mp3Files.Add(fn);
+				nums.Add(nums.Count);
+			}
+			// step 3: randomely swap two numbers in the list nums
+			Random rnd = new Random();
+			for (int i = 0; i < exchTimes; i++) swapPos(rnd.Next(nums.Count), rnd.Next(nums.Count));
+			// step 4: rename all files in the folder with an additional '_', for ex: .mp3 to mp3_
+			for (int i = 0; i < nums.Count; i++) File.Move(mp3Files[i], mp3Files[i] + "_");
+			// step 5: rename all files back to mp3 base on the the values in nums list
+			for (int i = 0; i < nums.Count; i++) File.Move(mp3Files[i] + "_", txtPath.Text + paddingTo3(nums[i]) + ".mp3");
+			MessageBox.Show("Files Shuffled Completely!!");
+		}
+
+		private void swapPos(int v1, int v2)
+		{
+			int tmp = nums[v1];
+			nums[v1] = nums[v2];
+			nums[v2] = tmp;
+		}
+
+		private string paddingTo3(int income)
+		{
+			if (income < 10) return "00" + income.ToString();
+			else if (income < 100) return "0" + income.ToString();
+			else return income.ToString();
+		}
+
+	}
+}
